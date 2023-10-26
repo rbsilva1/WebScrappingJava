eaf698bc04f3f59ede3040f12b11c95036eb997f
daniellucena1 <danieljlt23@gmail.com>
2023-10-26 18:52:38 -0300

    alterando a função obtemUltimoResultado

--- mavenBasico/src/main/java/br/upe/ResultadoMegaSena.java
+++ mavenBasico/src/main/java/br/upe/ResultadoMegaSena.java
@@ -4,7 +4,9 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 /**
  * Classe que obtém os números do último sorteio da mega-sena.
@@ -12,13 +14,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 public class ResultadoMegaSena {
     /** URL que possui as dezenas sorteadas. */
-    private final static String URL = "http://www1.caixa.gov.br/_newincludes/home_2011/resultado_megasena.asp";
-
-    /** Marcação inicial para extrair as dezenas do retorno HTML. */
-    private final static String MARCA_INICIAL_RETORNO_NAO_UTIL = "<div id='concurso_resultado'>";
-
-    /** Marcação final para extrair as dezenas do retorno HTML. */
-    private final static String MARCA_FINAL_RETORNO_NAO_UTIL = "</div>";
+    private final static String URL = "https://noticias.uol.com.br/loterias/mega-sena/";
 
     /**
      * Método que se conecta ao site da CEF para obter as dezenas do último sorteio.
@@ -27,38 +23,21 @@ public class ResultadoMegaSena {
      */
 
     public static String[] obtemUltimoResultado() {
-        // Criação do cliente HTTP que fará a conexão com o site
-        HttpClient httpclient = new DefaultHttpClient();
-        try {
-            // Definição da URL a ser utilizada
+        try (CloseableHttpClient httpclient = HttpClients.custom().disableRedirectHandling() // Desativa o tratamento de redirecionamento
+                .build()) {
             HttpGet httpget = new HttpGet(URL);
-
-            // Manipulador da resposta da conexão com a URL
-            ResponseHandler<String> responseHandler = new BasicResponseHandler();
-
-            // Resposta propriamente dita
-            String html = httpclient.execute(httpget, responseHandler);
-
-            // Retorno das dezenas, após tratamento
+            String html = EntityUtils.toString(httpclient.execute(httpget).getEntity());
             return obterDezenas(html);
-
         } catch (Exception e) {
-            // Caso haja erro, dispara exceção.
-            throw new RuntimeException("Um erro inesperado ocorreu.", e);
-
-        } finally {
-            // Destruição do cliente para liberação dos recursos do sistema.
-            httpclient.getConnectionManager().shutdown();
+            throw new RuntimeException("Um erro inesperado ocorreu !", e);
         }
     }
-
     /**
      * Tratamento da resposta HTML obtida pelo método obtemUltimoResultado().
      * 
      * @param html resposta HTML obtida
      * @return array de Strings, onde cada elemento é uma dezena sorteada.
      */
-
     private static String[] obterDezenas(String html) {
         // Posição inicial de onde começam as dezenas
         Integer parteInicial = html.indexOf(MARCA_INICIAL_RETORNO_NAO_UTIL) + MARCA_INICIAL_RETORNO_NAO_UTIL.length();
