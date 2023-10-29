fce62bfe45dfaec81dc946e7de57c450d992294e
Vitório Fernandes de Amorim <110642578+vitorio-fernandes@users.noreply.github.com>
2023-10-29 10:14:58 -0300

    Update App.java

--- mavenBasico/src/main/java/br/upe/App.java
+++ mavenBasico/src/main/java/br/upe/App.java
@@ -1,10 +1,19 @@
-package br.upe;
+package br.upe.mavenBasico;
 
 public class App {
-  public static void main(String[] args) {
-    String[] resultado = ResultadoMegaSena.obtemUltimoResultado();
-    for (String dezena : resultado) {
-      System.out.print(dezena + " ");
+    public static void main(String[] args) {
+        String[] dezenas = null;
+
+        while (dezenas == null) {
+            dezenas = ResultadoMegasena.obtemUltimoResultado();
+            if(dezenas != null){
+                System.out.println("Dezenas da Mega Sena:");
+                for (String dezena : dezenas) {
+                    System.out.print(dezena + " ");
+                }
+                System.out.println("\n");
+
+            }
+        }
     }
-  }
 }
