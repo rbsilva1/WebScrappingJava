836e273897d621c11f2e7d5ef5cafabce58d820f
Rodrigo Belarmino <rodrigo.belarmino17@gmail.com>
2023-10-29 23:27:18 -0300

    Removendo pastas inuteis e modificando versao do maven

--- mavenBasico/src/main/java/br/upe/ResultadoMegaSena.java
+++ mavenBasico/src/main/java/br/upe/ResultadoMegaSena.java
@@ -1,7 +1,5 @@
 package br.upe;
 
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
